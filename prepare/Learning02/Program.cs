a814392aa5ac91a2492f23b2ba2d088e706d9ed6
thatwascooladri <infothatwascool@gmail.com>
2024-09-24 09:05:33 +0800

    changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -17,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Adrian De Leon";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
